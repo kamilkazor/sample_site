7e37771e0f7d8db54a1e23194833dd17f7d7a081
kamilkazor <79405091+kamilkazor@users.noreply.github.com>
2021-10-03 22:56:54 +0200

    Added sticky property to Navbar, changed back display:none to .hide

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,31 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import logoIcon from "../images/logoIcon.svg"
 
 const Navbar = () => {
   const [hideLinks, setHideLinks] = useState(true);
+  const [sticky, setSticky] = useState(false);
+  const navRef = useRef()
+  const initialOffset = useRef(false);
 
+  //Hides and shows ul with links on mobile devices
   const hamburgerHandler = () => {
     setHideLinks(!hideLinks);
   }
 
+  //Adds and remover position fixed to Navbar
+  const scrollHandler = () => {
+    if(!initialOffset.current) initialOffset.current = navRef.current.offsetTop;
+    window.pageYOffset >= initialOffset.current ? setSticky(true) : setSticky(false);
+  }
+  useEffect(() => {
+    window.addEventListener("scroll", scrollHandler);
+    return () => {window.removeEventListener("scroll", scrollHandler)};
+  }, [])
+
+
   return (
-    <div className="Navbar">
+    <nav className={sticky ? "Navbar sticky" : "Navbar"} ref={navRef}>
       <div className="navContainer">
       <div className="logo"><img src={logoIcon} alt="logo"/><span>Sample Site</span></div>
       <div className="hamburger" onClick={hamburgerHandler}>☰</div>
@@ -21,7 +36,7 @@ const Navbar = () => {
         <li>link3</li>
       </ul>
       </div>
-    </div>
+    </nav>
   )
 }
 
