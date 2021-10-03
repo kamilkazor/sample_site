a4ed113e43dacf188b48a8f0f420da704f30f582
kamilkazor <79405091+kamilkazor@users.noreply.github.com>
2021-10-03 17:44:59 +0200

    Added ul and basic style to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,13 @@
 const Navbar = () => {
   return (
     <div className="Navbar">
-      Navbar
+      <div className="logo">Logo</div>
+      <ul>
+        <li>Home</li>
+        <li>link1</li>
+        <li>link2</li>
+        <li>link3</li>
+      </ul>
     </div>
   )
 }
