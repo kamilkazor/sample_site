d7353a9224462f816d38aec93ccd6f11f5a6d767
kamilkazor <79405091+kamilkazor@users.noreply.github.com>
2021-10-04 16:14:05 +0200

    Added animated burger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,21 +5,23 @@ import logoIcon from "../images/logoIcon.svg"
 const Navbar = ({headerRef}) => {
   const hideLinks = useRef(true);
   const [linksStyle, setLinksStyle] = useState(""); //Empty string as initial state prevents running animation on first render
+  const [burgerOpen, setBurgerOpen] = useState(false);
   const [sticky, setSticky] = useState(false);
   const navRef = useRef()
 
   //Hides and shows ul with links on mobile devices
-  const hamburgerHandler = () => {
+  const burgerHandler = () => {
+    setBurgerOpen(!burgerOpen);
     hideLinks.current = !hideLinks.current;
     hideLinks.current ? setLinksStyle("hide") : setLinksStyle("show");
   }
 
   //Adds and remover position fixed to Navbar
-  const scrollHandler = () => {
-    const initialNavOffset = headerRef.current.offsetTop + headerRef.current.clientHeight - navRef.current.clientHeight;
-    window.pageYOffset >= initialNavOffset ? setSticky(true) : setSticky(false);
-  }
   useEffect(() => {
+    const scrollHandler = () => {
+      const initialNavOffset = headerRef.current.offsetTop + headerRef.current.clientHeight - navRef.current.clientHeight;
+      window.pageYOffset >= initialNavOffset ? setSticky(true) : setSticky(false);
+    }
     window.addEventListener("scroll", scrollHandler);
     return () => {window.removeEventListener("scroll", scrollHandler)};
   }, [])
@@ -29,7 +31,9 @@ const Navbar = ({headerRef}) => {
     <nav className={sticky ? "Navbar sticky" : "Navbar"} ref={navRef}>
       <div className="navContainer">
       <div className="logo"><img src={logoIcon} alt="logo"/><span>Sample Site</span></div>
-      <div className="hamburger" onClick={hamburgerHandler}>☰</div>
+      <div onClick={burgerHandler} className={burgerOpen ? "menu open" : "menu"}>
+        <div className="burger"></div>
+      </div>
       <ul className={linksStyle}>
         <span><li>Home</li></span>
         <span><li>link1</li></span>
