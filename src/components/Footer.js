78bca7ab21c7293bbb753d771e4cdbef3280c94c
kamilkazor <79405091+kamilkazor@users.noreply.github.com>
2021-10-04 21:59:13 +0200

    Added content and styled footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,32 @@
+import Row from "./Row";
+
+import githubQR from "../images/githubQR.png";
+import phoneIcon from "../images/phoneIcon.svg";
+import envelopeIcon from "../images/envelopeIcon.svg";
+
 const Footer = () => {
   return (
     <footer className="Footer">
       <div className="footerContent">
-        Footer
+        <img className="githubQR" src={githubQR} alt="github QR Code"/>
+        <div className="infoBox">
+          <div>
+            <h3>Site created by Kamil Kazor</h3>
+            <p>To se more of my works checkout my github:</p>
+            <p><a href="https://github.com/kamilkazor">https://github.com/kamilkazor</a></p>
+          </div>
+          <div className="contact">
+            <h3>Contact:</h3>
+            <Row>
+              <img className="contactIcon" src={phoneIcon} alt="tel"/>
+              <p>+48 664 909 231</p>
+            </Row>
+            <Row>
+              <img className="contactIcon" src={envelopeIcon} alt="email"/>
+              <p>kkrk1994@gmail.com</p>
+            </Row>
+          </div>
+        </div>
       </div>
     </footer>
   )
---
+++ src/components/Row.js
@@ -0,0 +1,10 @@
+const Row = ({children}) => {
+  return (
+      <div className="row">
+        {children}
+      </div>
+    )
+  }
+  
+  
+  export default Row;
