71647832c81d19c28e7c1c59eb9cd57166cd433e
kamilkazor <79405091+kamilkazor@users.noreply.github.com>
2021-10-05 15:23:00 +0200

    Fixed layout issues on very small screens

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,22 +5,21 @@ const Home = () => {
   return (
     <div className="mainContainer">
       <main className="main">
-        <article className="about row">
-          <img className="aboutLogoImg" src={logoIcon} alt="logo"/>
-          <div className="aboutText">
+        <article id="aboutArticle" className="row">
+          <img id="aboutLogoImg" src={logoIcon} alt="logo"/>
+          <div className="textBox">
             <h2>About:</h2>
             <p>Sample Site is my web-dev training project.</p>
             <p>Please feel free to explore the site and see what it has to offer.</p>
           </div>
         </article>
-        <article className="responsive row">
-          <div className="responsiveText">
+        <article id="responsiveArticle" className="row">
+          <div className="textBox">
             <h2>Responsive design:</h2>
             <p>Flexible layout, sticky navigation bar and animated mobile menu...</p>
-            <p>The site was written the way to provide a nice experience on all devices.</p>
-            <p>Please resize the window or scan the QR Code with your phone to see all the features.</p>
+            <p>The site was written the way to provide a nice experience on all devices. Please resize the window or scan the QR Code with your phone to see all the features.</p>
           </div>
-          <img className="responsiveQR" src={sampleSiteQR} alt="logo"/>
+          <img id="responsiveQR" src={sampleSiteQR} alt="sample site QR code"/>
         </article>
       </main>
     </div>
