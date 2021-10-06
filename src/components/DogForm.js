0a7920b4ea92a583c62e1f08338e5d4bc8805382
kamilkazor <79405091+kamilkazor@users.noreply.github.com>
2021-10-06 04:18:27 +0200

    Added dynamic options to controlled select inputs

--- src/components/DogForm.js
+++ src/components/DogForm.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
 import { setBreedsData } from "../redux/dogSlice";
@@ -6,6 +6,11 @@ import { setBreedsData } from "../redux/dogSlice";
 const DogForm = () => {
   const breedsData = useSelector((store) => store.dog.breedsData);
   const dispatch = useDispatch();
+
+  const [breeds, setBreeds] = useState([]);
+  const [selectedBreed, setSelectedBreed] = useState("all");
+  const [subBreeds, setSubBreeds] = useState([])
+  const [selectedSubBreed, setSelectedSubBreed] = useState("all");
 
   //Fetching avaliable breeds form Dog API 
   useEffect(() => {
@@ -24,20 +29,28 @@ const DogForm = () => {
       .catch((err) => {
         console.log(err)
       })
-  },[])
+  },[]);
+  //Setting avaliable options for select inputs
+  useEffect(() => {
+    setBreeds(["all", ...Object.keys(breedsData)]);
+  },[breedsData])
+  useEffect(() => {
+    setSelectedSubBreed("all")
+    selectedBreed == "all" ? setSubBreeds(["all"]) : setSubBreeds(["all", ...breedsData[selectedBreed]])
+  },[selectedBreed])
 
 
 
   return (
     <div>
       <form>
-        <label htmlFor="breed">breed</label>
-        <select id="breed" name="breed">
-
+        <label htmlFor="breed">breed: </label>
+        <select id="breed" name="breed" value={selectedBreed} onChange={(e) => {setSelectedBreed(e.target.value)}}>
+          {breeds.map((breed) => <option key={breed} value={breed}>{breed}</option>)}
         </select>
-        <label htmlFor="subBreed">sub-breed</label>
-        <select id="subBreed" name="subBreed">
-          
+        <label htmlFor="subBreed">sub-breed: </label>
+        <select id="subBreed" name="subBreed" value={selectedSubBreed} onChange={(e) => {setSelectedSubBreed(e.target.value)}}>
+          {subBreeds.map((subBreed) => <option key={subBreed} value={subBreed}>{subBreed}</option>)}
         </select>
       </form>
     </div>
